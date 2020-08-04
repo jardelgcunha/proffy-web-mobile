aec978f74cb6af07db49df79f6657f067dc3c3e0
Jardel <developerprojectfullstack@gmail.com>
2020-08-04 05:46:57 -0300

    importante o global.css e add button

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import React from "react";
+
+import "./assets/styles/global.css";
+
+// JSX = Javascript + XML
 
 function App() {
   return (
     <div className="App">
       <h1>Hellor World!</h1>
+
+      <button type="button">Clica em mim</button>
     </div>
   );
 }
