56c73b133d286f1c42f95654709fa5e517eea06c
Jardel <developerprojectfullstack@gmail.com>
2020-08-05 07:13:57 -0300

    add props.children no page header

--- src/components/PageHeader/index.tsx
+++ src/components/PageHeader/index.tsx
@@ -21,6 +21,7 @@ const PageHeader: React.FC<PageHeaderProps> = (props) => {
 
       <div className="header-content">
         <strong>{props.title}</strong>
+        {props.children}
       </div>
     </header>
   );
