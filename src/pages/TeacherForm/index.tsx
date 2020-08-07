eedba19f37a8c1826a245071f4e1e23de102c023
Jardel <developerprojectfullstack@gmail.com>
2020-08-07 18:42:51 -0300

    add description do <PageHeader/> no TeacherForm

--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -3,11 +3,14 @@ import React from "react";
 import PageHeader from "../../components/PageHeader";
 
 function TeacherForm() {
-  return (
-    <div id="page-teacher-form" className="container">
-      <PageHeader title="Que incrível que você quer dar aulas." />
-    </div>
-  );
+	return (
+		<div id="page-teacher-form" className="container">
+			<PageHeader
+				title="Que incrível que você quer dar aulas."
+				description="O primeiro passo é preencher esse formúlario de inscrição"
+			/>
+		</div>
+	);
 }
 
 export default TeacherForm;
