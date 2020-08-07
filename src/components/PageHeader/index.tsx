d411bb52fb1624909953fbab207a669c71778fb4
Jardel <developerprojectfullstack@gmail.com>
2020-08-07 18:43:32 -0300

    config description com props no PageHeader

--- src/components/PageHeader/index.tsx
+++ src/components/PageHeader/index.tsx
@@ -8,25 +8,29 @@ import backIcon from "../../assets/images/icons/back.svg";
 import "./styles.css";
 
 interface PageHeaderProps {
-  title: string;
+	title: string;
+	description?: string;
 }
 
 const PageHeader: React.FC<PageHeaderProps> = (props) => {
-  return (
-    <header className="page-header">
-      <div className="top-bar-container">
-        <Link to="/">
-          <img src={backIcon} alt="Voltar" />
-        </Link>
-        <img src={logoImg} alt="Proffy" />
-      </div>
+	return (
+		<header className="page-header">
+			<div className="top-bar-container">
+				<Link to="/">
+					<img src={backIcon} alt="Voltar" />
+				</Link>
+				<img src={logoImg} alt="Proffy" />
+			</div>
 
-      <div className="header-content">
-        <strong>{props.title}</strong>
-        {props.children}
-      </div>
-    </header>
-  );
+			<div className="header-content">
+				<strong>{props.title}</strong>
+				{/* {props.description ? <p>{props.description}</p> : null} */}
+				{props.description && <p>{props.description}</p>}
+
+				{props.children}
+			</div>
+		</header>
+	);
 };
 
 export default PageHeader;
