fed1f951102b465f6023c64ade95d236ef2fcd54
Jardel <developerprojectfullstack@gmail.com>
2020-08-08 00:53:07 -0300

    set and configure the components on TheacherForm

--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 
 import PageHeader from "../../components/PageHeader";
+import Input from "../../components/Input";
+
+import warningIcom from "../../assets/images/icons/warning.svg";
+
+import "./styles.css";
 
 function TeacherForm() {
 	return (
@@ -9,6 +14,31 @@ function TeacherForm() {
 				title="Que incrível que você quer dar aulas."
 				description="O primeiro passo é preencher esse formúlario de inscrição"
 			/>
+
+			<main>
+				<fieldset>
+					<legend>Seus dados</legend>
+					<Input name="name" label="Nome completo" />
+					<Input name="avatar" label="Avatar" />
+					<Input name="whatsapp" label="Whatsapp" />
+				</fieldset>
+
+				<fieldset>
+					<legend>Sobre a aula</legend>
+					<Input name="subject" label="Matéria" />
+					<Input name="cost" label="Custo da sua hora por aula" />
+				</fieldset>
+
+				<footer>
+					<p>
+						<img src={warningIcom} alt="Aviso importante" />
+						Importante
+						<br />
+						Preencha todos os dados
+					</p>
+					<button type="button">Salvar cadastro</button>
+				</footer>
+			</main>
 		</div>
 	);
 }
