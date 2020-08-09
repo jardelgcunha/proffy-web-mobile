6c252a633a59d276671efacf6250e9a06fc0fc38
Jardel <developerprojectfullstack@gmail.com>
2020-08-09 16:28:33 -0300

    set total connections linked in whatsapp button

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import whatsappIcon from "../../assets/images/icons/whatsapp.svg";
 
 import "./styles.css";
+import api from "../../services/api";
 
 export interface Teacher {
 	id: number;
@@ -19,6 +20,11 @@ interface TeacherItemProps {
 }
 
 const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+	function createNewConnection() {
+		api.post("connections", {
+			user_id: teacher.id,
+		});
+	}
 	return (
 		<article className="teacher-item">
 			<header>
@@ -36,7 +42,11 @@ const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
 					Preço/hora
 					<strong>R$ {teacher.cost}</strong>
 				</p>
-				<a href={`https://wa.me/${teacher.whatsapp}`}>
+				<a
+					target="_blank"
+					onClick={createNewConnection}
+					href={`https://wa.me/${teacher.whatsapp}`}
+				>
 					<img src={whatsappIcon} alt="Whatsapp" />
 					Entrar em contato
 				</a>
