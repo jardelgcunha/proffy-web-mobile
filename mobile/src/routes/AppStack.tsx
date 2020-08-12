0a26abeb19805f2b8c9996b46e1f7c6c5967e762
jardel cunha <developerprojectfullstack@gmail.com>
2020-08-12 14:46:51 -0300

    create route for give-classes in mobile

--- mobile/src/routes/AppStack.tsx
+++ mobile/src/routes/AppStack.tsx
@@ -3,6 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import Landing from '../pages/Landing';
+import GiveClasses from '../pages/GiveClasses';
 
 const { Navigator, Screen } = createStackNavigator();
 
@@ -11,10 +12,10 @@ function AppStack() {
         <NavigationContainer>
             <Navigator screenOptions={{ headerShown:false }}>
                 <Screen name="Landing" component={Landing} />
+                <Screen name="GiveClasses" component={GiveClasses} />
             </Navigator>
         </NavigationContainer>
     )
-
 }
 
 export default AppStack;
