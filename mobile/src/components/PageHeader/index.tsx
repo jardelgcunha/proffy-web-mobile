26fd587fa8ccea0e861b7aa2c899643083b8efc1
jardel cunha <developerprojectfullstack@gmail.com>
2020-08-14 23:05:16 -0300

    add elemenst and filters in page header

--- mobile/src/components/PageHeader/index.tsx
+++ mobile/src/components/PageHeader/index.tsx
@@ -1,18 +1,23 @@
-import React from "react";
+import React, { ReactNode } from "react";
 import { View, Image, Text } from "react-native";
 import { BorderlessButton } from "react-native-gesture-handler";
+import { useNavigation } from "@react-navigation/native";
 
 import backIcon from "../../assets/images/icons/back.png";
 import logoIcon from "../../assets/images/logo.png";
 
 import styles from "./styles";
-import { useNavigation } from "@react-navigation/native";
 
 interface PageHeaderProps {
 	title: string;
+	headerRight?: ReactNode;
 }
 
-const PageHeader: React.FC<PageHeaderProps> = ({ title }) => {
+const PageHeader: React.FC<PageHeaderProps> = ({
+	title,
+	headerRight,
+	children,
+}) => {
 	const { navigate } = useNavigation();
 
 	function handleGoBack() {
@@ -29,7 +34,13 @@ const PageHeader: React.FC<PageHeaderProps> = ({ title }) => {
 				<Image source={logoIcon} resizeMode="contain" />
 			</View>
 
-			<Text style={styles.title}>{title}</Text>
+			<View style={styles.header}>
+				<Text style={styles.title}>{title}</Text>
+
+				{headerRight}
+			</View>
+
+			{children}
 		</View>
 	);
 };
