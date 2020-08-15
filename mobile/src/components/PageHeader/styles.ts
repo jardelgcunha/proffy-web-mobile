0952c0b1cdc1c7a53f29b0ec7cba8d6b4f019845
jardel cunha <developerprojectfullstack@gmail.com>
2020-08-14 23:05:33 -0300

    style elements and filters in page header

--- mobile/src/components/PageHeader/styles.ts
+++ mobile/src/components/PageHeader/styles.ts
@@ -2,11 +2,20 @@ import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
 	container: {
-		padding: 40,
+		padding: 30,
 		backgroundColor: "#8257e5",
+		// marginBottom: 60,
+		// height: 200,
+		// height: "100%",
 	},
 
 	topBar: {
+		flexDirection: "row",
+		alignItems: "center",
+		justifyContent: "space-between",
+	},
+
+	header: {
 		flexDirection: "row",
 		alignItems: "center",
 		justifyContent: "space-between",
@@ -16,9 +25,11 @@ const styles = StyleSheet.create({
 		fontFamily: "Archivo_700Bold",
 		color: "#FFF",
 		fontSize: 24,
-		lineHeight: 32,
+		lineHeight: 48,
 		// maxWidth: 160,
-		marginVertical: 40,
+		marginVertical: 8,
+		marginBottom: 10,
+		textAlign: "center",
 	},
 });
 
