20c8bbc39c446f5b96f8f74b75743505edd5be33
jardel cunha <developerprojectfullstack@gmail.com>
2020-08-14 23:08:04 -0300

    style the favorites

--- mobile/src/pages/Favorites/styles.ts
+++ mobile/src/pages/Favorites/styles.ts
@@ -1,10 +1,14 @@
 import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#f0f0f7'
-    },
-})
+	container: {
+		flex: 1,
+		backgroundColor: "#8257e5",
+	},
+
+	teacherList: {
+		marginTop: -10,
+	},
+});
 
 export default styles;
