1bac475583eb692dcbff6f46dfafc410cfabf535
jardel cunha <developerprojectfullstack@gmail.com>
2020-08-14 23:07:55 -0300

    create favorites

--- mobile/src/pages/Favorites/index.tsx
+++ mobile/src/pages/Favorites/index.tsx
@@ -1,14 +1,31 @@
 import React from "react";
-import { View } from "react-native";
+import { View, ScrollView, Text } from "react-native";
 
 import PageHeader from "../../components/PageHeader";
+import TeacherItem from "../../components/TeacherItem";
 
 import styles from "./styles";
 
 function Favorites() {
 	return (
 		<View style={styles.container}>
-			<PageHeader title="Meus proffys favoritos" />
+			<PageHeader title="Meus proffys favoritos">
+				{/* <Text>TESTE</Text> */}
+			</PageHeader>
+
+			<ScrollView
+				style={styles.teacherList}
+				contentContainerStyle={{
+					paddingHorizontal: 16,
+					paddingBottom: 16,
+				}}
+			>
+				<TeacherItem></TeacherItem>
+				<TeacherItem></TeacherItem>
+				<TeacherItem></TeacherItem>
+				<TeacherItem></TeacherItem>
+				<TeacherItem></TeacherItem>
+			</ScrollView>
 		</View>
 	);
 }
